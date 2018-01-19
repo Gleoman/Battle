1ab286c290ad6f4d1e25b9e6f398610e244e70a3
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-19 11:12:43 +0000

    Attack player 1

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -11,9 +11,10 @@ end
 feature 'Recording Hit Points' do
   scenario 'See player2 Hit Points' do
     sign_in_and_play
-    expect(page).to have_content("Player 2 Hit Points = 100")
+    expect(page).to have_content("#{$game.player2.name} Hit Points = 100")
   end
 end
+
 
 feature 'Attacking Player' do
   scenario 'Player 1 receives confirmation of attack on Player 2' do
@@ -26,6 +27,18 @@ feature 'Attacking Player' do
     sign_in_and_play
     click_button("P1_attack_P2")
     expect(page).to have_content("Ed takes damage of 10! #{$game.player2.hit_points}")
+  end
+
+  scenario 'Player 2 receives confirmation of attack on Player 1' do
+    sign_in_and_play
+    click_button("P2_attack_P1")
+    expect(page).to have_content("Attack Teddy")
+  end
+
+  scenario 'Player 2 reduces Player 1 Hit Points by attacking by 10' do
+    sign_in_and_play
+    click_button("P2_attack_P1")
+    expect(page).to have_content("Teddy takes damage of 10! #{$game.player1.hit_points}")
   end
 
 end
